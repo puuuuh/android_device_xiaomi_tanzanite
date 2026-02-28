3e1f123c3aee21222fa822efadf294fb31608ad6
Nathan <nathannxx@proton.me>
2026-02-28 18:15:54 +0000

    tanzanite: Mikudayoo~
    Signed-off-by: Nathan <nathannxx@proton.me>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,4 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_tanzanite.mk
+    $(LOCAL_DIR)/miku_tanzanite.mk
--- miku_tanzanite.mk
+++ miku_tanzanite.mk
@@ -8,14 +8,14 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit_only.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common MikuUI stuff.
+$(call inherit-product, vendor/miku/build/product/miku_product.mk)
 
 # Inherit from tanzanite device
 $(call inherit-product, device/xiaomi/tanzanite/device.mk)
 
 PRODUCT_DEVICE := tanzanite
-PRODUCT_NAME := lineage_tanzanite
+PRODUCT_NAME := miku_tanzanite
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := 24117RN76O
 PRODUCT_MANUFACTURER := xiaomi
